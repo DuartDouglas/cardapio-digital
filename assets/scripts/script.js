051d57c5a7d4916a308de364a0ed0c30bcf613ef
perkles <douglas_duart@outlook.com>
2023-11-24 15:31:50 -0300

    resumo e redução de qtd no item, teste de ver pedido

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -69,8 +69,6 @@ const createMenu = () => {
          const addNewItem = document.createElement('div');
          addNewItem.className = 'addNewItem';
 
-         //const form = document.createElement('form');
-
          const label = document.createElement('p');
          label.classList = "label";
          label.textContent = 'Alguma observação?';
@@ -86,58 +84,67 @@ const createMenu = () => {
          const buttons = document.createElement('div');
          buttons.classList = 'buttons';
 
-         const btnDeleteItem = document.createElement('button');
+         const span = document.createElement('span');
+         span.textContent = `Não adicionado`;
+
+         /* const btnDeleteItem = document.createElement('button');
          btnDeleteItem.classList = 'btnDeleteItem';
 
          const svgBtnDeleteItem = document.createElementNS("http://www.w3.org/2000/svg", "svg");
          const useBtnDeleteItem = document.createElementNS("http://www.w3.org/2000/svg", "use");
-         useBtnDeleteItem.setAttributeNS("http://www.w3.org/1999/xlink", "href", "assets/images/icons.svg#btnDeleteItem");
+         useBtnDeleteItem.setAttributeNS("http://www.w3.org/1999/xlink", "href", "assets/images/icons.svg#btnDeleteItem"); */
 
          const div = document.createElement('div');
          div.classList = 'btnsQtd';
 
          const buttonMinus = document.createElement('button');
-         buttonMinus.textContent = '-';
+         buttonMinus.innerHTML = '<svg> <use xlink:href="assets/images/icons.svg#minus"/></svg>';
+         buttonMinus.addEventListener('click', () => {
+            removeFromCart(item);
+            span.textContent = `${order.filter(i => i === item).length} und`;
 
-         const span = document.createElement('span');
-         span.textContent = '1';
+            if (order.filter(i => i === item).length < 1) {
+               buttonMinus.style.display = "none";
+            } else {
+               buttonMinus.style.display = "flex";
+            }
+         });
 
-         const buttonPlus = document.createElement('button');
-         buttonPlus.textContent = '+';
-
+         buttonMinus.style.display = "none";
+         
          const buttonAdd = document.createElement('button');
          buttonAdd.classList = 'btnAdd';
-         buttonAdd.textContent = 'Adicionar';
+
+         buttonAdd.innerHTML = `Adicionar <svg><use xlink:href="assets/images/icons.svg#plus" /></svg>`;
          buttonAdd.onclick = () => {
             addToCart(item);
+            span.textContent = `${order.filter(i => i === item).length} und`;
+
+            if (order.filter(i => i === item).length < 1) {
+               buttonMinus.style.display = "none";
+            } else {
+               buttonMinus.style.display = "flex";
+            }
          }
 
          addNewItem.appendChild(label);
          addNewItem.appendChild(textarea);
          addNewItem.appendChild(buttons);
-         buttons.appendChild(btnDeleteItem);
-         btnDeleteItem.appendChild(svgBtnDeleteItem);
-         svgBtnDeleteItem.appendChild(useBtnDeleteItem);
+         buttons.appendChild(span);
+
          buttons.appendChild(div);
          div.appendChild(buttonMinus);
-         div.appendChild(span);
-         div.appendChild(buttonPlus);
+
          buttons.appendChild(buttonAdd);
 
-         //addNewItem.appendChild(form);
          itemMenu.appendChild(addNewItem);
 
-         // precisa conferir se existe algum item na sacola para ser exibido o modal
+         // se não existe nenhum item na sacola exibido no modal a msg
          modalSummaryOrder.classList.add('show');
          let qtdOrderSummary = document.getElementById("qtdOrderSummary");
-         qtdOrderSummary.innerHTML = "Nenhum item no carrinho";
-
-         // Trabalhando no formulário
-         /*  btnDeleteItem.addEventListener('click', () => {
-             form.preventDefault();
-             addNewItem.classList.remove('show');
-             btnAddItem[i].style.display = 'block';
-          }); */
+         if (order.length < 1) {
+            qtdOrderSummary.innerHTML = "Nenhum item no carrinho";
+         }
       });
 
    });
@@ -146,114 +153,87 @@ const createMenu = () => {
 function addToCart(item) {
    order.push(item);
    total += item.preco;
-   console.log(total);
+   //console.log(total);
    updateOrderSummary();
 }
 
+function removeFromCart(item) {
+   const index = order.findIndex(i => i === item);
+
+   if (index !== -1) {
+      order.splice(index, 1);
+      total -= item.preco;
+      updateOrderSummary();
+      //checkout(); // Atualiza o resumo do pedido ao remover um item
+   }
+}
+
 function updateOrderSummary() {
-   //let orderSummary = document.getElementById("orderSummary");
    let qtdOrderSummary = document.getElementById("qtdOrderSummary");
    let totalPrice = document.getElementById('totalPrice');
    if (order.length < 1) {
       qtdOrderSummary.innerHTML = "Nenhum item no carrinho";
+      totalPrice.innerHTML = "";
    } else if (order.length < 2) {
       qtdOrderSummary.innerHTML = "Total sem entrega";
       totalPrice.innerHTML = "R$ " + total.toFixed(2) + " <span>/ " + order.length + " item</span>";
    } else {
       totalPrice.innerHTML = "R$ " + total.toFixed(2) + " <span>/ " + order.length + " itens</span>";
    }
-
-   /* if (order.length < 2) {
-      orderSummary.innerHTML = "R$ " + total.toFixed(2) + " <span>/ " + order.length +" item</span>";   
-   } else {
-      orderSummary.innerHTML = "R$ " + total.toFixed(2) + " <span>/ " + order.length +" itens</span>";   
-   } */
 }
 
-/* function checkout() {
-   // Aqui você pode adicionar lógica para finalizar a compra, como enviar o pedido para um servidor, etc.
-   alert("Compra realizada com sucesso!");
-   order = [];
-   total = 0;
-   updateOrderSummary();
-} */
+function checkout() {
+   const checkoutContainer = document.getElementById('checkoutContainer');
+   checkoutContainer.innerHTML = ""; // Limpa o conteúdo anterior
 
-window.onload = createMenu;
+   if (order.length === 0) {
+      checkoutContainer.innerHTML = "Nenhum item no carrinho";
+   } else {
+      const checkoutTitle = document.createElement('h2');
+      checkoutTitle.textContent = "Resumo do Pedido";
 
+      const itemList = document.createElement('ul');
 
+      // Cria um objeto para armazenar a contagem de cada item e suas observações
+      const itemDetails = {};
 
-//============================================================
-// CÓDIGO ANTIGO =============================================
-//============================================================
-/* const btnAddItem = document.querySelectorAll('.btnAddItem');
-const modalSummaryOrder = document.getElementById('modalSummaryOrder');
+      // Agrupa os itens e suas observações no carrinho
+      order.forEach(item => {
+         const itemName = item.nome;
+         const itemObservations = item.observacoes || "Sem observações";
 
+         if (!itemDetails[itemName]) {
+            itemDetails[itemName] = {
+               quantity: 0,
+               observations: itemObservations,
+            };
+         }
 
-for (let i = 0; i < btnAddItem.length; i++) {
-   btnAddItem[i].addEventListener('click', () => {
-      btnAddItem[i].style.display = 'none';
-      const addNewItem = document.querySelectorAll('.addNewItem')[i];
-      addNewItem.classList.add('show');
-
-      const form = document.createElement('form');
-
-      const label = document.createElement('label');
-      label.textContent = 'Alguma observação?';
-
-      const textarea = document.createElement('textarea');
-      textarea.setAttribute('cols', '30');
-      textarea.setAttribute('rows', '3');
-      textarea.setAttribute('placeholder', 'Ex: Tirar cheddar, etc...');
-
-      const buttons = document.createElement('div');
-      buttons.classList = 'buttons';
-
-      const btnDeleteItem = document.createElement('button');
-      btnDeleteItem.classList = 'btnDeleteItem';
-
-      const svgBtnDeleteItem = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      const useBtnDeleteItem = document.createElementNS("http://www.w3.org/2000/svg", "use");
-      useBtnDeleteItem.setAttributeNS("http://www.w3.org/1999/xlink", "href", "assets/images/icons.svg#btnDeleteItem");
-
-      const div = document.createElement('div');
-      div.classList = 'btnsQtd';
-
-      const buttonMinus = document.createElement('button');
-      buttonMinus.textContent = '-';
-
-      const span = document.createElement('span');
-      span.textContent = '1';
-
-      const buttonPlus = document.createElement('button');
-      buttonPlus.textContent = '+';
-
-      const buttonAdd = document.createElement('button');
-      buttonAdd.classList = 'btnAdd';
-      buttonAdd.textContent = 'Adicionar';
-
-      form.appendChild(label);
-      form.appendChild(textarea);
-      form.appendChild(buttons);
-      buttons.appendChild(btnDeleteItem);
-      btnDeleteItem.appendChild(svgBtnDeleteItem);
-      svgBtnDeleteItem.appendChild(useBtnDeleteItem);
-      buttons.appendChild(div);
-      div.appendChild(buttonMinus);
-      div.appendChild(span);
-      div.appendChild(buttonPlus);
-      buttons.appendChild(buttonAdd);
-
-      addNewItem.appendChild(form);
-
-      // precisa conferir se existe algum item na sacola para ser exibido o modal
-      modalSummaryOrder.classList.add('show');
-
-      // Trabalhando no formulário
-      btnDeleteItem.addEventListener('click', () => {
-         form.preventDefault();
-         addNewItem.classList.remove('show');
-         btnAddItem[i].style.display = 'block';
+         itemDetails[itemName].quantity++;
       });
 
-   });
-}; */
+      // Adiciona cada item, sua quantidade e observações ao resumo
+      for (const itemName in itemDetails) {
+         const { quantity, observations } = itemDetails[itemName];
+
+         const listItem = document.createElement('li');
+         listItem.innerHTML = `<strong>${quantity}x ${itemName} - </strong>Observações: ${observations}`;
+
+         itemList.appendChild(listItem);
+      }
+
+      const totalContainer = document.createElement('div');
+      totalContainer.textContent = `Total: R$ ${total.toFixed(2)}`;
+
+      checkoutContainer.appendChild(checkoutTitle);
+      checkoutContainer.appendChild(itemList);
+      checkoutContainer.appendChild(totalContainer);
+   }
+
+   modalSummaryOrder.classList.add('show');
+}
+
+const seeOrder = document.getElementById('seeOrder')
+seeOrder.addEventListener('click', checkout);
+
+window.onload = createMenu;
